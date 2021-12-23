7288397a6710acd3ad370bc56969d19ee018924f
sodes-proxy <sochoaosuna@gmail.com>
2021-12-22 20:49:18 -0600

    start of project

---
+++ src/ChessPiece.java
@@ -0,0 +1,32 @@
+/**
+ *
+ */
+public class ChessPiece {
+    private int x;
+    private int y;
+    String name;
+    boolean isWhite;
+    public ChessPiece(int x, int y,String name, boolean isWhite) {
+        this.x = x;
+        this.y = y;
+        this.isWhite = isWhite;
+        this.name=name;
+    }
+    public void move(int x,int y, ChessPiece [][] board){
+    }
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
