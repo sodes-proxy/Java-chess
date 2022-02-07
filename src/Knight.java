aec18ae258b0bf45e8a2a2c633d0a3c3ec85473a
sodes-proxy <sochoaosuna@gmail.com>
2022-02-07 17:34:08 -0600

    implemented Knight movement

--- src/Knight.java
+++ src/Knight.java
@@ -1,4 +1,17 @@
 public class Knight extends ChessPiece{
+    @Override
+    public boolean canMove(int x, int y, ChessPiece[][] board) {
+        if((x==this.getX()+1 ) && y==this.getY()-2) return true;
+        if(x==this.getX()+2 && y==this.getY()-1) return true;
+        if(x==this.getX()-1 && y==this.getY()-2) return true;
+        if(x==this.getX()-2 && y==this.getY()-1) return true;
+        if(x==this.getX()+1 && y==this.getY()+2) return true;
+        if(x==this.getX()+2 && y==this.getY()+1) return true;
+        if(x==this.getX()-1 && y==this.getY()+2) return true;
+        if(x==this.getX()-2 && y==this.getY()+1) return true;
+        return false;
+    }
+
     public Knight(int x, int y, boolean isWhite) {
         super(x, y);
         this.name="Horse";
