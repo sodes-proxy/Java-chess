0db7988c2fd990930b4c10dd0d979a5193ffab9b
sodes-proxy <sochoaosuna@gmail.com>
2022-02-07 17:33:50 -0600

    implemented bishop movement

--- src/Bishop.java
+++ src/Bishop.java
@@ -4,4 +4,25 @@ public class Bishop extends ChessPiece{
         this.name="Bishop";
         this.isWhite=isWhite;
     }
+
+    @Override
+    public boolean canMove(int x, int y, ChessPiece[][] board) {
+        int size= board.length;
+        //diagonal movement
+        for(int i=0;i<size;i++){
+            int top=this.getY()-i;
+            int right=this.getX()+i;
+            int bottom=this.getY()+i;
+            int left=this.getX()-i;
+            //top right
+            if((x==right && right<size) && (y==top && top>0)) return true;
+            //bottom left
+            if((x==left && left>0) && (y==bottom && bottom<size)) return true;
+            //top left
+            if((x==left && left>0) && (y==top && top>0)) return true;
+            //bottom right
+            if((x==right && right<size) && (y==bottom && bottom<size)) return true;
+        }
+        return false;
+    }
 }
