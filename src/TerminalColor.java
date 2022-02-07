4fe296081d6d8e99bba792d760a30e4c06a5e8b4
sodes-proxy <sochoaosuna@gmail.com>
2022-02-07 13:38:16 -0600

    Changed Terminal colors to an enum

---
+++ src/TerminalColor.java
@@ -0,0 +1,8 @@
+public enum TerminalColor {
+    ANSI_RESET("\u001B[0m"), ANSI_WHITE("\u001B[37m"), ANSI_BLACK("\u001B[30m"), ANSI_RED("\u001B[31m"), ANSI_GREEN("\u001B[32m"), ANSI_BLACK_BACKGROUND("\u001B[40m"), ANSI_WHITE_BACKGROUND("\u001B[47m");
+    final String color;
+
+    TerminalColor(String color) {
+        this.color = color;
+    }
+}
